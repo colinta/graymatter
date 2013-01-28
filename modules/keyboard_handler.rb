225af328153cda9b20bc014a9ecc47f40cf95f65
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-27 19:40:04 -0700

    KeyboardHandler refactors

--- modules/keyboard_handler.rb
+++ modules/keyboard_handler.rb
@@ -1,30 +1,36 @@
 module GM
-  module KeyboardHandler
-    def keyboard_handle_start(scroll_view)
-      NSNotificationCenter.defaultCenter.addObserver(self,
-              selector: :'keyboard_did_show:',
-              name: UIKeyboardDidShowNotification,
-              object: nil)
-      NSNotificationCenter.defaultCenter.addObserver(self,
-              selector: :keyboard_will_hide,
-              name: UIKeyboardWillHideNotification,
-              object: nil)
+  module KeyboardHandlerModule
+
+    def prepare_keyboard_handler(scroll_view)
       @keyboard_handler_scroll_view = scroll_view
     end
 
-    def keyboard_handle_stop
+    def keyboard_handler_start
+      NSNotificationCenter.defaultCenter.addObserver(self,
+              selector: :'keyboard_handler_keyboard_did_show:',
+              name: UIKeyboardDidShowNotification,
+              object: nil)
+      NSNotificationCenter.defaultCenter.addObserver(self,
+              selector: :keyboard_handler_keyboard_will_hide,
+              name: UIKeyboardWillHideNotification,
+              object: nil)
+
+      if KeyboardState.visible?
+        keyboard_handler_keyboard_did_show(KeyboardState.last_notification)
+      end
+    end
+
+    def keyboard_handler_stop
       NSNotificationCenter.defaultCenter.removeObserver(self, name: UIKeyboardDidShowNotification, object:nil)
       NSNotificationCenter.defaultCenter.removeObserver(self, name: UIKeyboardWillHideNotification, object:nil)
     end
 
-    def keyboard_did_show(notification)
+    def keyboard_handler_keyboard_did_show(notification)
       scroll_view = @keyboard_handler_scroll_view
-      bottom = scroll_view.convertPoint([0, scroll_view.bounds.size.height], toView:nil).y + scroll_view.contentOffset.y
-      window UIApplication.sharedApplication.keyWindow || UIApplication.sharedApplication.windows[0]
-      inset_from_bottom = window.bounds.size.height - bottom
+      kbd_rect = notification.userInfo[UIKeyboardFrameEndUserInfoKey].CGRectValue
+      kbd_rect = App.window.convertRect(kbd_rect, toView:scroll_view)
 
-      kbd_height = notification.userInfo[UIKeyboardFrameBeginUserInfoKey].CGRectValue.size.height
-      kbd_height -= inset_from_bottom
+      kbd_height = kbd_rect.size.height
       return if kbd_height < 0
 
       insets = [0, 0, kbd_height, 0]
@@ -39,7 +45,7 @@ module GM
       end
     end
 
-    def keyboard_will_hide
+    def keyboard_handler_keyboard_will_hide
       insets = [0, 0, 0, 0]
       scroll_view = @keyboard_handler_scroll_view
       scroll_view.contentInset = insets
