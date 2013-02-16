bfb2ffe1d59fee8e71bbec8026d24e8656ccd4c6
Colin T.A. Gray <colinta@gmail.com>
2013-02-16 08:20:06 -0700

    modal view and spinner fixes

--- lib/graymatter/modules/hide_show_modal.rb
+++ lib/graymatter/modules/hide_show_modal.rb
@@ -1,4 +1,13 @@
 module GM
+  class ModalView < UIView
+    attr_accessor :spinner
+
+    def layoutSubviews
+      modal_size = bounds.size
+      spinner.center = [modal_size.width / 2, modal_size.height / 2]
+    end
+  end
+
   # A modal overlay.
   module HideShowModal
     class << self
@@ -7,18 +16,24 @@ module GM
     end
 
     # call this method from your controller's `viewDidLoad` method
-    def prepare_hide_show_modal
+    def prepare_hide_show_modal(target=nil)
+      @hide_show_modal_target = target || App.window
+
       unless HideShowModal.modal_view
-        modal_view = UIView.alloc.initWithFrame(App.window.bounds)
+        modal_view = ModalView.new
         modal_view.backgroundColor = :black.uicolor(0.5)
-        spinner = UIActivityIndicatorView.large
-        spinner.center = [modal_view.bounds.width / 2, modal_view.bounds.height / 2]
-        spinner.startAnimating
-        modal_view << spinner
+        modal_view.autoresizingMask = :full.uiautoresizingmask
+        modal_view.spinner = UIActivityIndicatorView.large
+        modal_view.spinner.center = [modal_view.bounds.width / 2, modal_view.bounds.height / 2]
+        modal_view.spinner.startAnimating
+        modal_view << modal_view.spinner
         HideShowModal.modal_view = modal_view
       end
-      unless HideShowModal.modal_view.isDescendantOfView(App.window)
-        App.window << HideShowModal.modal_view
+
+      unless HideShowModal.modal_view.superview == @hide_show_modal_target
+        @hide_show_modal_target << HideShowModal.modal_view
+        HideShowModal.modal_view.frame = @hide_show_modal_target.bounds
+        HideShowModal.modal_view.setNeedsLayout
       end
 
       HideShowModal.modal_view.alpha = 0.0
@@ -37,7 +52,7 @@ module GM
     def show_modal
       @timer.invalidate if @timer
 
-      App.window.bringSubviewToFront(HideShowModal.modal_view)
+      @hide_show_modal_target.bringSubviewToFront(HideShowModal.modal_view)
       unless HideShowModal.modal_is_visible
         FuncTools.CFMain {
           HideShowModal.modal_view.alpha = 0.0
