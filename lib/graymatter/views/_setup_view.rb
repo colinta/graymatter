f9047e99a778f178f96f3c93f2ded3a7cba86193
Colin T.A. Gray <colinta@gmail.com>
2013-03-18 18:17:27 -0600

    SetupView

---
+++ lib/graymatter/views/_setup_view.rb
@@ -0,0 +1,34 @@
+module GM
+  module SetupView
+
+    def init
+      initWithFrame(CGRectZero)
+    end
+
+    def initWithFrame(frame)
+      super.tap do
+        _setup
+      end
+    end
+
+    def awakeFromNib
+      _setup
+    end
+
+    # if @_view_setup is nil, returns nil.  Next time, though, it will return true
+    def _setup_once
+      @_view_setup.tap {
+        @_view_setup = true
+      }
+    end
+
+    def _setup
+      return if _setup_once
+      setup
+    end
+
+    def setup
+    end
+
+  end
+end
