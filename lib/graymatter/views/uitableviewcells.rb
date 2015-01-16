20207d3e37d33ba8219c34aa2d6af4f86b061ac5
Colin T.A. Gray <colinta@gmail.com>
2015-01-16 13:27:03 -0700

    remove teacup code

--- lib/graymatter/views/uitableviewcells.rb
+++ lib/graymatter/views/uitableviewcells.rb
@@ -1,57 +1,23 @@
 module GM
-  CellStylesheet = Teacup::Stylesheet.new do
-    style :cell_label,
-      background: :clear,
-      color: '#526691',
-      font: :system.uifont(12),
-      alignment: :right
-    if device_is? iPhone
-      style :cell_label,
-        frame: [[0, 15], [67, 15]]
-    else
-      raise "todo: make this the right size for an iPad"
-      style :cell_label,
-        frame: [[0, 15], [67, 15]]
-    end
-
-    style :cell_input,
-      color: :black,
-      font: :bold.uifont(15),
-      borderStyle: :none,
-      edgeInsets: [11.5, 3, 0, 0],
-      autoresizing: :fill,
-      frame: [[67, 0], [230, 45]]
-
-    style :long_cell_label, extends: :cell_label,
-      frame: [[0, 15], [180, 15]]
-
-    style :cell_switch,
-      frame: [[209, 8], [79, 27]]
-  end
-
-  class GMCellClass < UITableViewCell
-    class << self
-      attr_accessor :stylesheet
-
-      # override the default stylesheet by assigning it to
-      # `TextFieldCellClass.stylesheet` or `BooleanCellClass`.stylesheet
-      def stylesheet
-        @stylesheet ||= CellStylesheet
-      end
-    end
-  end
-
-  class TextFieldCellClass < GMCellClass
+  class TextFieldCellClass < UITableViewCell
     attr_accessor :textLabel, :input
 
     def initWithStyle(style, reuseIdentifier:identifier)
       super(:value2.uitablecellstyle, reuseIdentifier:identifier).tap do
-        self.stylesheet = self.class.stylesheet
+        @textLabel = UILabel.alloc.initWithFrame([[0, 15], [67, 15]])
+        @textLabel.backgroundColor = :clear.uicolor
+        @textLabel.textColor = '#526691'.uicolor
+        @textLabel.font = :system.uifont(12)
+        @textLabel.textAlignment = :right.uitextalignment
+        self.contentView.addSubview(@textLabel)
 
-        layout(self.contentView, :cell) do
-          @textLabel = self.subview(UILabel, :cell_label)
-          @input = self.subview(InsetTextField, :cell_input)
-        end
+        @input = InsetTextField.alloc.initWithFrame([[67, 0], [230, 45]])
+        @input.textColor = :black.uicolor
+        @input.font = :bold.uifont(15)
+        @input.borderStyle = :none.uiborderstyle
+        @input.edgeInsets = [11.5, 3, 0, 0]
+        @input.autoresizingMask = :fill.uiautoresizemask
+        self.contentView.addSubview(@input)
       end
     end
 
@@ -62,18 +28,22 @@ module GM
   end
 
 
-  class BooleanCellClass < GMCellClass
+  class BooleanCellClass < UITableViewCell
     attr_accessor :textLabel, :input
 
     def initWithStyle(style, reuseIdentifier:identifier)
       super(:value2.uitablecellstyle, reuseIdentifier:identifier).tap do
-        self.stylesheet = self.class.stylesheet
-
-        layout(self.contentView, :cell) do
-          @textLabel = self.subview(UILabel, :long_cell_label)
-          @input = self.subview(UISwitch, :cell_switch)
-        end
         self.selectionStyle = UITableViewCellSelectionStyleNone
+
+        @textLabel = UILabel.alloc.initWithFrame([[0, 15], [180, 15]])
+        @textLabel.backgroundColor = :clear.uicolor
+        @textLabel.textColor = '#526691'.uicolor
+        @textLabel.font = :system.uifont(12)
+        @textLabel.textAlignment = :right.uitextalignment
+        self.contentView.addSubview(@textLabel)
+
+        @input = UISwitch.alloc.initWithFrame([[209, 8], [79, 27]])
+        self.contentView.addSubview(@input)
       end
     end
   end
